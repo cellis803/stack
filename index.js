e87ffe6c22aaf1609be6b3cd522bd1a2fd210391
cellis803 <cellis803@gmail.com>
2016-09-06 15:03:16 -0400

    added method stubs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+
+var stackObject = {
+
+    stack : [],
+
+    push : function (obj) {
+
+    },
+
+    pop : function() {
+        return null;
+    },
+
+    peek : function() {
+        return 'test';
+    }
+}
+
+console.log(stackObject.peek());
+
